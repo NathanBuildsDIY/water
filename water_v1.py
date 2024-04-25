51751437aeee49211b07cc3c0d6f0fff258a3b52
NathanBuildsDIY <165212390+NathanBuildsDIY@users.noreply.github.com>
2024-04-25 14:47:45 -0500

    Add files via upload
    Add city and rain water as inputs.

--- water_v1.py
+++ water_v1.py
@@ -13,11 +13,14 @@ args = parser.parse_args()
 #pump = AngularServo(10, initial_angle=0, min_angle=0, max_angle=180, min_pulse_width=4/10000, max_pulse_width=25/10000,pin_factory=factory)
 #pump = PWMLED(12, initial_value = 0, pin_factory = factory) #only if factory works - pi zero
 
+cityWater = LED(27)
+rainWater = LED(22)
+rainBarrelFull = Button(26)
 pump = LED(17)
 if args.zone == 1:
-  zoneValve = LED(27)
+  zoneValve = LED(23)
 if args.zone == 2:
-  zoneValve = LED(22)
+  zoneValve = LED(24)
   
 now = datetime.datetime.now()
 print("starting pump now for these seconds: ",args.seconds)
@@ -26,9 +29,19 @@ print(now)
 #pump.blink(1,1,1,1,2,True)
 zoneValve.on()
 time.sleep(0.5) #let valve open before we pump
-pump.on()
-time.sleep(args.seconds)
-pump.off()
+if rainBarrelFull.is_pressed:
+  rainWater.on()
+  time.sleep(0.5)
+  pump.on()
+  time.sleep(args.seconds)
+  pump.off()
+  time.sleep(0.5)
+  rainWater.off()
+else:
+  cityWater.on()
+  time.sleep(args.seconds)
+  cityWater.off()
+  time.sleep(0.5)
 zoneValve.off()
 now = datetime.datetime.now()
 print("turning off pump now for zone: ",args.zone)
